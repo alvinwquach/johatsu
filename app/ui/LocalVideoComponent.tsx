4c8161f372afb2dad93c1b405bfa733f814049c7
Alvin Quach <alvinwquach@gmail.com>
2025-09-15 15:57:31 -0700

    Add gif and local video

---
+++ app/ui/LocalVideoComponent.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+export default function LocalVideoComponent() {
+  return (
+    <div className="w-full max-w-3xl mt-12 aspect-video">
+      <video
+        className="w-full h-full rounded-lg shadow-lg object-cover"
+        autoPlay
+        muted
+        loop
+        playsInline
+        controls={false}
+      >
+        <source
+          src="/videos/D0bdDAFhmwgLPcYpbHD4rJH8AwoBruYg666YxW9le_g.mp4"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+}

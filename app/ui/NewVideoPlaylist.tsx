bb97336243965f01158e47058e32ebeeb5aea3a5
Alvin Quach <alvinwquach@gmail.com>
2025-10-12 08:25:44 -0700

    Add bells in santafe and government cheese

---
+++ app/ui/NewVideoPlaylist.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+function NewVideoPlaylist() {
+  return (
+    <div
+      style={{
+        position: "relative",
+        paddingBottom: "56.25%",
+        height: 0,
+        overflow: "hidden",
+      }}
+    >
+      <iframe
+        src="https://www.youtube.com/embed/xuXm-AwMO4c?autoplay=1&mute=1&loop=1&playlist=xuXm-AwMO4c,SXKB-HpKWUI"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+      />
+    </div>
+  );
+}
+
+export default NewVideoPlaylist;
